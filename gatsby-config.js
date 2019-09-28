05688141d362533d4978f193be0981839db6f368
Justin Aiello <justin@nomadicsoftware.com>
2019-09-28 12:55:53 -0400

    Setting up gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,31 @@
  */
 
 module.exports = {
-  /* Your site config here */
-}
+  siteMetaData: {
+    title: 'USNWC Gatsby Site',
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // CommonMark mode (default: true)
+        commonmark: true,
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // Pedantic mode (default: true)
+        pedantic: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+  ],
+};
